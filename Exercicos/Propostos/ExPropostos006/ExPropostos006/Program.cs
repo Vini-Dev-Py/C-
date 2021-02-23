6fac112a5a7c7a4bc5d7c0280133c7782b8ba953
Vinicius Guilherme Batista <62727555+Vini-Dev-Py@users.noreply.github.com>
2021-02-23 14:45:36 -0300

    C#

--- Exercicos/Propostos/ExPropostos006/ExPropostos006/Program.cs
+++ Exercicos/Propostos/ExPropostos006/ExPropostos006/Program.cs
@@ -12,10 +12,10 @@ namespace ExPropostos006
             // A) a área do triangulo retângulo que tem A por base e C altura. 
             // B) a área do círculo de raio C. (pi = 3.14159)
             // C) a área do trapézio que tem A e B por bases e C por altura.
-            // D) a área do quadrado que tem dado B.
+            // D) a área do quadrado que tem lado B.
             // E) a área do retângulo que tem lados A e B
 
-            double PI = 3.14159
+            double PI = 3.14159;
 
             string[] vet = Console.ReadLine().Split(" ");
 
@@ -38,8 +38,30 @@ namespace ExPropostos006
             // B) 
             // Fórmula A = π . r2
 
-            double Circulo = (PI * (Math.Pow(c, c)));
+            double Circulo = (PI * (Math.Pow(c, 2)));
 
+            Console.WriteLine("Circulo: {0}", Circulo.ToString("F4", CultureInfo.InvariantCulture));
+
+            // C)
+            // Fórmula A = ((B + b). h) / 2
+
+            double Trapezio = ((a + b) * c) / 2;
+
+            Console.WriteLine("Trapezio: {0}", Trapezio.ToString("F4", CultureInfo.InvariantCulture));
+
+            // D)
+            // Fórmula A = L.L
+
+            double quadrado = Math.Pow(b, 2);
+
+            Console.WriteLine("Quadrado: {0}", quadrado.ToString("F4", CultureInfo.InvariantCulture));
+
+            // E) 
+            // Fórmula A = b . h
+
+            double retangulo = a * b;
+
+            Console.WriteLine("Retangulo: {0}", retangulo.ToString("F4", CultureInfo.InvariantCulture));
         }
     }
 }
