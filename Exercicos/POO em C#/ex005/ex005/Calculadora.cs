fbda94f0ca856a37041555ef89866b1913f59745
Vinicius Guilherme Batista <62727555+Vini-Dev-Py@users.noreply.github.com>
2021-02-24 10:56:36 -0300

    C#

---
+++ Exercicos/POO em C#/ex005/ex005/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex005 {
+    class Calculadora
+    {
+        static double Pi = 3.14;
+        static double Circuferencia(double r)
+        {
+            return 2.0 * Pi * r;
+        }
+
+        static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+        }
+
+    }
+}
